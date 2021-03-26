a258104cabc70f422e53524945e945a53127adeb
ajperlis07 <ajperlis07@gmail.com>
2021-03-26 12:23:35 -0400

    model associations complete

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -1,3 +1,4 @@
 class Meme < ApplicationRecord
+  has_many :favorites
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    has_many :memes
+    has_many :memes, through: :favorites
+    has_many :favorites
 end
